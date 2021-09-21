e4661dc6633bc77b0c17bdb13ab601bf5f217e71
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2021-09-21 12:34:26 -0300

    Teste de conexão com Firebase
    - Adicionada uma linha de código para testar a conexão com o Firebase;
    - A linha foi comentada, não terá uso na versão final.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Home.dart';
 
-void main() => runApp(const MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Home(),
-));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  /* FirebaseFirestore.instance
+    .collection("usuarios")
+    .doc("001")
+    .set({
+      "nome" : "SampleText"
+    }); */
+
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
